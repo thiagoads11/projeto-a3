9b551a45cb033671e493b4b42d9d7dcefa1bdf31
Thiago Menezes <thmenezesdev@gmail.com>
2024-04-04 21:14:41 -0300

    add main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const criarNovoParticipante = () => {
+  return 'ola'
+
+
+}
+
+const atualizarLista = () => {
+  //Pegar informação do HTML
+
+  // Substituir informção do HTML
+  document
+  .querySelector('tbody') 
+  .innerHTML = criarNovoParticipante()
+}
